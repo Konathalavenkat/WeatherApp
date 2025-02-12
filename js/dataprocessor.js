17dee93415cd677b7c7b236ddb4af1a6a6642ca0
Konathalavenkat <venkatsivaff@gmail.com>
2025-02-12 13:03:15 +0530

    Added Worker Threads

---
+++ js/dataprocessor.js
@@ -0,0 +1,118 @@
+
+onmessage = function(event){
+    const {forecastData,history} = event.data;
+    console.log(forecastData,history,"from worker")
+    const hourlydata = [];
+    const dailydata = [];
+      const todayhourlydata = [];
+      dailydata.push(`
+        <div id="history-container">
+            <h3>History</h3>
+            <div id="History">`)
+      for (let i = 0; i < Math.min(3,history.forecast.forecastday.length); i++) {
+        const d = history.forecast.forecastday[i];
+        dailydata.push(`
+                <div class="day">
+                    <div>
+                        <img src="./assets/${getWeatherFromCode(
+                          d.day.condition.code
+                        )}.png" width="100%" loading="lazy">
+                    </div>
+                    <div>
+                        <h6>${d.date}</h5>
+                        <h6>${d.day.condition.text}</h6>
+                        <h6>Max temp: ${d.day.maxtemp_c}°C</h6>
+                        <h6>Min temp: ${d.day.mintemp_c}°C</h6>
+                    </div>
+                </div>
+                `);
+      }
+      dailydata.push(`    </div>
+        </div>
+        <div id="forecast-container">
+            <h3>Forecast</h3>
+            <div id="Forecast">`)
+      for (
+        let i = Number(history.location.localtime.substring(11, 13));
+        i < Math.min(24,forecastData.forecast.forecastday[0].hour.length);
+        i++
+      ) {
+        const hourdata = forecastData.forecast.forecastday[0].hour[i];
+        todayhourlydata.push(`
+                <div class="hourly">
+                    <div>
+                        <img src="./assets/${
+                          getWeatherFromCode(hourdata.condition.code) +
+                          (hourdata.is_day === 1 ? "" : "_night")
+                        }.png" width="100%" loading="lazy">
+                    </div>
+                    <div>
+                        <h5>${hourdata.time.substring(11, 16)}</h5>
+                        <h6>${hourdata.condition.text}</h6>
+                        <h6>Temp : ${hourdata.temp_c}°C</h6>
+                    </div>
+                </div>
+                `);
+      }
+      hourlydata.push(todayhourlydata);
+      for (let i = 0; i < Math.min(4,forecastData.forecast.forecastday.length); i++) {
+        const d = forecastData.forecast.forecastday[i];
+        dailydata.push(`
+                <div class="day" id="${d.date}" day="${i}">
+                    <div>
+                        <img src="./assets/${getWeatherFromCode(
+                          d.day.condition.code
+                        )}.png" width="100%" loading="lazy">
+                    </div>
+                    <div>
+                        <h6>${d.date}</h6>
+                        <h6>${d.day.condition.text}</h6>
+                        <h6>Max temp: ${d.day.maxtemp_c}°C</h6>
+                        <h6>Min temp: ${d.day.mintemp_c}°C</h6>
+                    </div>
+                </div>
+                `);
+        if (i != 0) {
+          const temphourlydata = [];
+
+          for (let hour = 0; hour < Math.min(24,forecastData.forecast.forecastday[i].hour.length); hour++) {
+            const hourdata = forecastData.forecast.forecastday[i].hour[hour];
+            temphourlydata.push(`
+                        <div class="hourly">
+                            <div>
+                                <img src="./assets/${
+                                  getWeatherFromCode(hourdata.condition.code) +
+                                  (hourdata.is_day === 1 ? "" : "_night")
+                                }.png" width="100%" loading="lazy" >
+                            </div>
+                            <div>
+                                <h5>${hourdata.time.substring(11, 16)}</h5>
+                                <h6>${hourdata.condition.text}</h6>
+                                <h6>Temp : ${hourdata.temp_c}°C</h6>
+                            </div>
+                        </div>
+                    `);
+          }
+          hourlydata.push(temphourlydata);
+        }
+      }
+      dailydata.push(`        </div>
+        </div>`);
+        postMessage({dailydata,hourlydata});
+}
+
+function getWeatherFromCode(code) {
+  if (code <= 1000) {
+    return "sunny";
+  } else if (code == 1030) {
+    return "foggy";
+  } else if (code <= 1117) {
+    return "cloudy";
+  } else if (code <= 1171) {
+    return "foggy";
+  } else if (code <= 1264) {
+    return "rainy";
+  } else {
+    return "thunder";
+  }
+}
--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ window.onload = () => {
 
   window.localStorage.setItem("Location", "Bengaluru");
 
-  const key = "7f275b4a47ef4dd3843155639252801";
+  const key = "d7086282fbbf40308c254148251102";
 
   let showdata = false;
-  const hourlydata = [];
+  let Hourlydata = [];
 
   function getLocation() {
     if (navigator.geolocation) {
@@ -32,7 +32,6 @@ window.onload = () => {
 
   function toggledata(){
     showdata = !showdata;
-    console.log(document.main)
     if(showdata){
       document.getElementById('forecast-history').style.display = 'flex';
       document.getElementById('toggler').innerText = 'Hide daywise data'; 
@@ -66,7 +65,6 @@ window.onload = () => {
       }
       const data = await response.json();
       window.localStorage.setItem("Location", location);
-      console.log(data);
       document.getElementById("details").innerHTML = `
             <h4>${data.location.name + ", " + data.location.region}</h4>
             <h5>${data.location.localtime}</h5>
@@ -147,110 +145,19 @@ window.onload = () => {
       ]);
       const forecastData = await forecastResponse.json();
       const history = await historyResponse.json();
-      console.log(history);
-      console.log(forecastData, history);
       const div = document.getElementById("hourly-forecast");
       const div2 = document.getElementById("forecast-history");
-      hourlydata.splice(0, hourlydata.length);
-      const dailydata = [];
-      const todayhourlydata = [];
-      dailydata.push(`
-        <div id="history-container">
-            <h3>History</h3>
-            <div id="History">`)
-      for (let i = 0; i < Math.min(3,history.forecast.forecastday.length); i++) {
-        const d = history.forecast.forecastday[i];
-        dailydata.push(`
-                <div class="day">
-                    <div>
-                        <img src="./assets/${getWeatherFromCode(
-                          d.day.condition.code
-                        )}.png" width="100%" loading="lazy">
-                    </div>
-                    <div>
-                        <h6>${d.date}</h5>
-                        <h6>${d.day.condition.text}</h6>
-                        <h6>Max temp: ${d.day.maxtemp_c}°C</h6>
-                        <h6>Min temp: ${d.day.mintemp_c}°C</h6>
-                    </div>
-                </div>
-                `);
+      const worker = new Worker("./js/dataprocessor.js");
+      worker.onmessage = (event) => {
+        const {dailydata , hourlydata } = event.data;
+        div2.innerHTML = dailydata.join("");
+        div.innerHTML = "";
+        document.getElementById("Hourly-text").innerText = '';
+        Hourlydata = hourlydata;
+        setEvents();
+        worker.terminate();
       }
-      dailydata.push(`    </div>
-        </div>
-        <div id="forecast-container">
-            <h3>Forecast</h3>
-            <div id="Forecast">`)
-      for (
-        let i = Number(history.location.localtime.substring(11, 13));
-        i < Math.min(24,forecastData.forecast.forecastday[0].hour.length);
-        i++
-      ) {
-        const hourdata = forecastData.forecast.forecastday[0].hour[i];
-        todayhourlydata.push(`
-                <div class="hourly">
-                    <div>
-                        <img src="./assets/${
-                          getWeatherFromCode(hourdata.condition.code) +
-                          (hourdata.is_day === 1 ? "" : "_night")
-                        }.png" width="100%" loading="lazy">
-                    </div>
-                    <div>
-                        <h5>${hourdata.time.substring(11, 16)}</h5>
-                        <h6>${hourdata.condition.text}</h6>
-                        <h6>Temp : ${hourdata.temp_c}°C</h6>
-                    </div>
-                </div>
-                `);
-      }
-      hourlydata.push(todayhourlydata);
-      for (let i = 0; i < Math.min(4,forecastData.forecast.forecastday.length); i++) {
-        const d = forecastData.forecast.forecastday[i];
-        dailydata.push(`
-                <div class="day" id="${d.date}" day="${i}">
-                    <div>
-                        <img src="./assets/${getWeatherFromCode(
-                          d.day.condition.code
-                        )}.png" width="100%" loading="lazy">
-                    </div>
-                    <div>
-                        <h6>${d.date}</h6>
-                        <h6>${d.day.condition.text}</h6>
-                        <h6>Max temp: ${d.day.maxtemp_c}°C</h6>
-                        <h6>Min temp: ${d.day.mintemp_c}°C</h6>
-                    </div>
-                </div>
-                `);
-        if (i != 0) {
-          const temphourlydata = [];
-
-          for (let hour = 0; hour < Math.min(24,forecastData.forecast.forecastday[i].hour.length); hour++) {
-            const hourdata = forecastData.forecast.forecastday[i].hour[hour];
-            temphourlydata.push(`
-                        <div class="hourly">
-                            <div>
-                                <img src="./assets/${
-                                  getWeatherFromCode(hourdata.condition.code) +
-                                  (hourdata.is_day === 1 ? "" : "_night")
-                                }.png" width="100%" loading="lazy" >
-                            </div>
-                            <div>
-                                <h5>${hourdata.time.substring(11, 16)}</h5>
-                                <h6>${hourdata.condition.text}</h6>
-                                <h6>Temp : ${hourdata.temp_c}°C</h6>
-                            </div>
-                        </div>
-                    `);
-          }
-          hourlydata.push(temphourlydata);
-        }
-      }
-      dailydata.push(`        </div>
-        </div>`);
-      div2.innerHTML = dailydata.join("");
-      div.innerHTML = "";
-      document.getElementById("Hourly-text").innerText = '';
-      setEvents();
+      worker.postMessage({forecastData,history});
       document.getElementById('loader').style.display = 'none'
     } catch (err) {
         document.getElementById('loader').style.display = 'none'
@@ -259,22 +166,19 @@ window.onload = () => {
   }
 
   function sethourlydata(event) {
-    console.log(event);
     const textdata = document.getElementById("Hourly-text");
     const date = this.id;
     if (!date) return;
     const day = Number(this.getAttribute("day"));
     textdata.textContent = "Hourly ForeCast of " + date;
     const div = document.getElementById("hourly-forecast");
-    console.log(hourlydata, day, this);
-    div.innerHTML = hourlydata[day].join("");
+    div.innerHTML = Hourlydata[day].join("");
   }
 
   function setEvents() {
     const classNames = document.getElementsByClassName("day");
     Array.from(classNames).forEach(function (element) {
       element.addEventListener("click", sethourlydata);
-      // console.log(element);
     });
   }
 
@@ -297,7 +201,6 @@ window.onload = () => {
   const inputbox = document.getElementById("Location-box");
 
   search.addEventListener("click", () => {
-    console.log(inputbox.value);
     if (inputbox.value.length > 2) {
       document.getElementById('loader').style.display = 'flex';
       updateCurrentData(inputbox.value);
