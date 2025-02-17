0f826ed8756b389f2e827618dd0e2ac8234d7ea0
Konathalavenkat <venkatsivaff@gmail.com>
2025-02-17 11:14:53 +0530

    Added Debounce for Autocomplete

---
+++ js/Utils.js
@@ -0,0 +1,42 @@
+
+/**
+ * Utility Function that fetches the data from api
+ * @param {String} url - Url of the API
+ * @returns data of the api response
+ */
+export async function fetchFromAPI(url){
+    try{
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error(response);
+        }
+        const data = await response.json();
+        return data;
+    }
+    catch(err){
+        console.log("Error fetching data:",err);
+        throw err;
+    }
+}
+
+
+/**
+ * Function that returns Generalized Weather name from condition code
+ * @param {Number} code - code Representing the Weather Condition
+ * @returns generalized weather name
+ */
+export function getWeatherFromCode(code) {
+    if (code <= 1000) {
+      return "sunny";
+    } else if (code == 1030) {
+      return "foggy";
+    } else if (code <= 1117) {
+      return "cloudy";
+    } else if (code <= 1171) {
+      return "foggy";
+    } else if (code <= 1264) {
+      return "rainy";
+    } else {
+      return "thunder";
+    }
+  }
--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
+import { fetchFromAPI ,getWeatherFromCode} from "./Utils.js";
+
 window.onload = () => {
 
+  /** Setting Default Location */
   window.localStorage.setItem("Location", "Bengaluru");
 
+  /** Api Key */
   const key = "d7086282fbbf40308c254148251102";
 
+  /** flag used to toggle daywise data in the UI */
   let showdata = false;
+
   let Hourlydata = [];
 
+  /** Variable used to store Debounce TimeOut in the Search Function */
+  let debounceTimeOut;
+
+  /**
+   * Gets Locations from the User using geolocation
+   */
   function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        showPosition,
+        updatePosition,
         getDefaultLocation
       );
     } else {
@@ -18,18 +30,28 @@ window.onload = () => {
     }
   }
 
-  function getDefaultLocation(err) {
-    console.log("Location Access Not Given");
-    window.localStorage.setItem("Location", " Location 28.7041,77.1025");
-  }
-
-  function showPosition(position) {
+  /**
+   * Updating the Location in LocalStoragae
+   */
+  function updatePosition(position) {
     window.localStorage.setItem(
       "Location",
       position.coords.latitude + "," + position.coords.longitude
     );
   }
 
+  /**
+   * Error Handling Function for geolocation.getCurrentPosition()
+   * @param {Error} err - Error Returned by the geolocation.getCurrentPosition()
+   */
+  function getDefaultLocation(err) {
+    console.log("Location Access Not Given");
+    window.localStorage.setItem("Location", "Location 28.7041,77.1025");
+  }
+
+  /**
+   * Toggles the Daywise data UI component
+   */
   function toggledata(){
     showdata = !showdata;
     if(showdata){
@@ -48,24 +70,13 @@ window.onload = () => {
     }
   }
 
-  async function updateCurrentData(inputlocation) {
-    if (inputlocation) {
-      window.localStorage.setItem("InputLocation", inputlocation);
-    }
-    const location = inputlocation
-      ? inputlocation
-      : window.localStorage.getItem("Location");
-    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${
-      location ? location : "28.7041,77.1025"
-    }&aqi=yes`;
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      const data = await response.json();
-      window.localStorage.setItem("Location", location);
-      document.getElementById("details").innerHTML = `
+  /**
+   * Function that returns the HTML code for Current Component
+   * @param {json} data - Response data of the current api
+   * @returns HTML code of the Current Component
+   */
+  function CurrentUIComponent(data){
+    return `
             <h4>${data.location.name + ", " + data.location.region}</h4>
             <h5>${data.location.localtime}</h5>
             <h6>Weather : ${data.current.condition.text}<h6>
@@ -90,16 +101,37 @@ window.onload = () => {
               <button class="btn btn-primary mt-2 ml-5" id="toggler">${showdata ? 'Hide daywise data' : 'Show daywise data'} </button>
 
             </div>
-            `;
-      document.getElementById(
-        "body"
-      ).style.backgroundImage = `url(./assets/${getWeatherFromCode(
-        data.current.condition.code
-      )}_bg-min.jpg)`;
-      document.getElementById("current-img").src = `./assets/${
-        getWeatherFromCode(data.current.condition.code) +
-        (data.current.is_day === 1 ? "" : "_night")
-      }.png`;
+            `
+  }
+
+  /**
+   * Updated the Background image of the body according to the weather
+   * @param {Number} code - Code representing the Weather Condition
+   */
+  function updateBackgroundImageOfBody(code){
+    document.body.style.backgroundImage = `url(./assets/${getWeatherFromCode(
+      code
+    )}_bg-min.jpg)`;
+  }
+
+  /**
+   * Updates the Current Weather Conditions into UI
+   * @param {String} inputlocation - Location Given by the User
+   */
+  async function updateCurrentData(inputlocation) {
+    if (inputlocation) {
+      window.localStorage.setItem("InputLocation", inputlocation);
+    }
+
+    const location = inputlocation ? inputlocation : window.localStorage.getItem("Location");
+    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${location ? location : "28.7041,77.1025"}&aqi=yes`;
+
+    try {
+      const data = await fetchFromAPI(url);
+      window.localStorage.setItem("Location", location);
+      document.getElementById("details").innerHTML = CurrentUIComponent(data);
+      updateBackgroundImageOfBody(data.current.condition.code);
+      document.getElementById("current-img").src = `./assets/${getWeatherFromCode(data.current.condition.code) +(data.current.is_day === 1 ? "" : "_night")}.png`;
       document.getElementById("toggler").addEventListener('click',toggledata);
     } catch (err) {
       document.getElementById('loader').style.display = 'none'
@@ -107,47 +139,46 @@ window.onload = () => {
     }
   }
 
-  function getWeatherFromCode(code) {
-    if (code <= 1000) {
-      return "sunny";
-    } else if (code == 1030) {
-      return "foggy";
-    } else if (code <= 1117) {
-      return "cloudy";
-    } else if (code <= 1171) {
-      return "foggy";
-    } else if (code <= 1264) {
-      return "rainy";
-    } else {
-      return "thunder";
-    }
-  }
-
+  /**
+   * Formats the Date
+   * @param {String} date 
+   * @returns Formatted Date String
+   */
   function getDateFormat(date) {
     return (
       date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
     );
   }
 
-  async function updateHourlyForecast() {
+  /**
+   * Updates the History and Forecast of Weather into the UI
+   */
+  async function updateHistoryAndForecast() {
     try {
       const location = window.localStorage.getItem("Location");
-      const url = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location ? location : "28.7041,77.1025"}&days=4&aqi=yes&alerts=yes`;
+      
+      const forecastUrl = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location ? location : "28.7041,77.1025"}&days=4&aqi=yes&alerts=yes`;
+      
       const today = new Date();
       let prevday = new Date();
       prevday.setDate(prevday.getDate() - 3);
 
-      const historyurl = `https://api.weatherapi.com/v1/history.json?key=${key}&q=${location ? location : "28.7041,77.1025"}&dt=${getDateFormat(prevday)}&end_dt=${getDateFormat(today)}`;
-
+      const historyUrl = `https://api.weatherapi.com/v1/history.json?key=${key}&q=${location ? location : "28.7041,77.1025"}&dt=${getDateFormat(prevday)}&end_dt=${getDateFormat(today)}`;
+      
       const [forecastResponse, historyResponse] = await Promise.all([
-        fetch(url),
-        fetch(historyurl),
+        fetch(forecastUrl),
+        fetch(historyUrl),
       ]);
+
       const forecastData = await forecastResponse.json();
       const history = await historyResponse.json();
       const div = document.getElementById("hourly-forecast");
       const div2 = document.getElementById("forecast-history");
+
+      
+      //Worker For Processing HTML Component from the History and Forecast Reponse 
       const worker = new Worker("./js/dataprocessor.js");
+
       worker.onmessage = (event) => {
         const {dailydata , hourlydata } = event.data;
         div2.innerHTML = dailydata.join("");
@@ -157,6 +188,7 @@ window.onload = () => {
         setEvents();
         worker.terminate();
       }
+
       worker.postMessage({forecastData,history});
       document.getElementById('loader').style.display = 'none'
     } catch (err) {
@@ -165,7 +197,10 @@ window.onload = () => {
     }
   }
 
-  function sethourlydata(event) {
+  /**
+   * Updates the Hourly data
+   */
+  function sethourlydata() {
     const textdata = document.getElementById("Hourly-text");
     const date = this.id;
     if (!date) return;
@@ -175,6 +210,9 @@ window.onload = () => {
     div.innerHTML = Hourlydata[day].join("");
   }
 
+  /**
+   * Adds EventListeners for Every Day component
+   */
   function setEvents() {
     const classNames = document.getElementsByClassName("day");
     Array.from(classNames).forEach(function (element) {
@@ -182,63 +220,72 @@ window.onload = () => {
     });
   }
 
+  /**
+   * Updates the Search with the clicked autocomplete field
+   * @param {String} text - text of the autocomplete 
+   */
   function updateinput(text){
     const inputbox = document.getElementById('Location-box');
     inputbox.value = this.innerText ? this.innerText : text;
     document.getElementById('list-autocomplete').innerHTML = "";
     document.getElementById('loader').style.display = 'flex';
     updateCurrentData(inputbox.value);
-    updateHourlyForecast();
+    updateHistoryAndForecast();
     inputbox.value = "";
     document.getElementById('list-autocomplete').innerHTML = "";
   }
 
   getLocation();
   updateCurrentData();
-  updateHourlyForecast();
+  updateHistoryAndForecast();
 
   const search = document.getElementById("search");
   const inputbox = document.getElementById("Location-box");
 
+  //Event Listener for Search Button
   search.addEventListener("click", () => {
     if (inputbox.value.length > 2) {
       document.getElementById('loader').style.display = 'flex';
       updateCurrentData(inputbox.value);
-      updateHourlyForecast();
+      updateHistoryAndForecast();
       inputbox.value = "";
       document.getElementById('list-autocomplete').innerHTML = "";
     }
   });
 
+  //Event Listener for Search Bar
   inputbox.addEventListener('keydown',async function(event){
     if(event.key == "Enter"){
       if (inputbox.value.length > 2) {
         document.getElementById('loader').style.display = 'flex';
         updateCurrentData(inputbox.value);
-        updateHourlyForecast();
+        updateHistoryAndForecast();
         inputbox.value = "";
         document.getElementById('list-autocomplete').innerHTML = "";
       }
     }
     else{
-      const autocompleteapi = `https://api.weatherapi.com/v1/search.json?key=${key}&q=${inputbox.value}`;
-      try{
-        const searchresponse = await fetch(autocompleteapi);
-        if(!searchresponse.ok){
-          throw new Error(searchresponse.response);
-        }
-        const cities =  await searchresponse.json();
-        data = cities.map((val,i) => (`<li tabindex="0"> ${val.name} ${val.region ? ', ' : '' }${val.region} </li>`))
+      clearTimeout(debounceTimeOut);
+      debounceTimeOut = setTimeout(async ()=>{
+        const autocompleteapi = `https://api.weatherapi.com/v1/search.json?key=${key}&q=${inputbox.value}`;
+        try{
+          const searchresponse = await fetch(autocompleteapi);
+          if(!searchresponse.ok){
+            throw new Error(searchresponse.response);
+          }
+          const cities =  await searchresponse.json();
+          data = cities.map((val,i) => (`<li tabindex="0"> ${val.name} ${val.region ? ', ' : '' }${val.region} </li>`))
 
-        document.getElementById('list-autocomplete').innerHTML = data.join(" ");
-        for(const option of document.getElementById('list-autocomplete').children){
-          option.addEventListener('click',updateinput);
-          option.addEventListener('keydown',function(event){if(event.key == "Enter") updateinput(this.innerText)});  
+          document.getElementById('list-autocomplete').innerHTML = data.join(" ");
+          for(const option of document.getElementById('list-autocomplete').children){
+            option.addEventListener('click',updateinput);
+            option.addEventListener('keydown',function(event){if(event.key == "Enter") updateinput(this.innerText)});  
+          }
         }
-      }
-      catch(err){
-        console.log(err);
-      }
+        catch(err){
+          console.log(err);
+        }
+      },500)
     }
   })
   
