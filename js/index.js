e930c588d906a5525d31afe583b247866a2a1ad1
venkat <venkatsivaff@gmail.com>
2025-05-28 00:36:31 +0530

    updated key

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ window.onload = () => {
   window.localStorage.setItem("Location", "Bengaluru");
 
   /** Api Key */
-  const key = "20f87a57f6184afc85653427252502";
+  const key = "dc2e462aa2bd4f79a0a121249251805";
 
   /** flag used to toggle daywise data in the UI */
   let showdata = false;
